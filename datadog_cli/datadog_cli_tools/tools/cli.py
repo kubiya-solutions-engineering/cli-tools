1005c9dc9d79e43cb4fe4ce390656d6fb9e94258
michagonzo77 <michael.gonz.fl@gmail.com>
2025-07-10 17:49:03 -0400

    fixes

--- datadog_cli/datadog_cli_tools/tools/cli.py
+++ datadog_cli/datadog_cli_tools/tools/cli.py
@@ -64,7 +64,7 @@ EOF
             # Test list monitors
             echo "Testing list monitors..."
             sleep 1
-            dog monitor show_all --app-key ${DD_APP_KEY} --api-key ${DD_API_KEY} --api-host ${DD_SITE}
+            dog monitor show_all --application-key ${DD_APP_KEY} --api-key ${DD_API_KEY} --api_host ${DD_SITE}
             sleep 2
             echo "✅ List monitors test successful"
             
