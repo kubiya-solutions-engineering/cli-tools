b06c6c35091bfb75ffba0a3abeb298536978368a
michagonzo77 <michagonzo77@gmail.com>
2025-07-22 10:45:39 -0400

    fixes

--- observe_cli/observe_cli_tools/tools/base.py
+++ observe_cli/observe_cli_tools/tools/base.py
@@ -53,7 +53,7 @@ class ObserveCLITool(Tool):
             icon_url=OBSERVE_CLI_ICON_URL,
             type="docker",
             secrets=["OBSERVE_API_KEY", "OBSERVE_CUSTOMER_ID"],
-            env=["OBSERVE_DATASET_ID"],
+            env=[],
             with_volumes=[{
                 "path": "/workspace/observe-data",
                 "name": "observe-data"
